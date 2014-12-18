a921914c51217d454f5085db7c8c3fe6d616d5bc
Jeffrey Jackson <jj@autolean.com>
2014-12-18 09:53:52 -0500

    Initial Commit

---
+++ ALAppleSearchAPI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ALAppleSearchAPI
+//
+//  Created by Jeffrey Jackson on 12/18/14.
+//  Copyright (c) 2014 AutoLean, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
